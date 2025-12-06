931ab49b78e9c431e75bae94328773d837cb575e
nathan <nlepacik@gmail.com>
2025-12-06 03:31:24 -0700

    day 1 cursed one line solution

--- day1/Main.java
+++ day1/Main.java
@@ -1,9 +1,11 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
-public class Main {
-    static int dial = 50;
-    public static void main(String args[]) throws Exception {
-        System.out.println(Files.lines(Path.of("input.txt")).mapToInt(line -> ((dial += (Integer.parseInt(line.substring(1)) * (line.charAt(0) == 'L' ? -1 : 1))) % 100 == 0) ? 1 : 0).sum());
-    }
+void main() throws Exception {
+	System.out.println("Part 1: " + ((BiFunction<Stream<String>, Integer[], Integer>) ((stream, dial) -> stream.mapToInt(line -> ((dial[0] += (Integer.parseInt(line.substring(1)) * (line.charAt(0) == 'L' ? -1 : 1))) % 100 == 0) ? 1 : 0).sum())).apply(Files.lines(Path.of("input.txt")), new Integer[]{50}));
+	System.out.println("Part 2: " + ((BiFunction<Stream<String>, Integer[], Integer>) ((stream, dial) -> stream.mapToInt(line -> ((Function<Integer, Integer>) (d -> (int) (line.charAt(0) == 'L' ? IntStream.rangeClosed(d, d + Integer.parseInt(line.substring(1)) - 1) : IntStream.rangeClosed(d - Integer.parseInt(line.substring(1)) + 1, d)).filter(n -> n % 100 == 0).count())).apply(dial[0] += Integer.parseInt(line.substring(1)) * (line.charAt(0) == 'R' ? 1 : -1))).sum())).apply(Files.lines(Path.of("input.txt")), new Integer[]{50}));
 }
